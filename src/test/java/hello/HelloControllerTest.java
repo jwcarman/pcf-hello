10f2e8a9cac20a8e9685e15990c5f1b74c85cbf3
James Carman <jwcarman@gmail.com>
2017-09-12 10:46:20 -0400

    Adding failing test.

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -2,12 +2,10 @@ package hello;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-
 public class HelloControllerTest {
 
     @Test
     public void testSomething() {
-        assertTrue(false);
+
     }
 }
