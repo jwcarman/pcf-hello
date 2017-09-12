9541da4ed95dd579f69eb618d93d0398cf8d036d
James Carman <jwcarman@gmail.com>
2017-09-12 10:53:26 -0400

    Adding actuator.

---
+++ src/main/java/hello/HelloHealthCheck.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloHealthCheck implements HealthIndicator {
+//----------------------------------------------------------------------------------------------------------------------
+// HealthIndicator Implementation
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public Health health() {
+        return Health.up().status("We're cool and the gang!").build();
+    }
+}
