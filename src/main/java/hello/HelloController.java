c3b2bf5590d32d22f3edd6b8a310d82c16ec1344
James Carman <jwcarman@gmail.com>
2017-09-15 16:44:35 -0400

    Removing extra stuff.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,20 +1,14 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 public class HelloController {
 
-    @Value("${message:Hello default}")
-    private String message;
-
     @RequestMapping("/")
     public String index() {
-        return message;
+        return "Hello, PCF!";
     }
 
 }
