7b38454da7092891e81539e1c1e958552ef6c425
James Carman <jwcarman@gmail.com>
2017-09-14 23:32:04 -0400

    Fixed discovery/config.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,20 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
-public class HelloController
-{
+public class HelloController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
     @RequestMapping("/")
     public String index() {
-        return "Hello, Cloud Foundry!";
+        return message;
     }
 
 }
