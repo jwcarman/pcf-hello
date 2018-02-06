4bb6966d470147fd31de0cd673105036a04b6a72
James Carman <james.carman@cengage.com>
2018-02-05 23:09:19 -0500

    Adding unit test.

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -2,10 +2,13 @@ package hello;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HelloControllerTest {
 
     @Test
     public void testSomething() {
-
+        final HelloController controller = new HelloController();
+        assertEquals("Hello, PCF!", controller.index());
     }
 }
