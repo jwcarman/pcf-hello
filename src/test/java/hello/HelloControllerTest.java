1833bf32f928a97850a06f518dfebb72ace8f476
James Carman <james.carman@cengage.com>
2018-02-05 21:26:43 -0500

    Test

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -2,11 +2,14 @@ package hello;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HelloControllerTest {
 
     @Test
     public void testSomething() {
         final HelloController helloController = new HelloController();
+        assertEquals("Hello, PCF!", helloController.index());
     }
 
 }
